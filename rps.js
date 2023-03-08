49dbfb09fa7c1693c0789bf3589696121be24703
gabe-san <gsantosx98@gmail.com>
2023-03-07 21:17:41 -0800

    basic ui elements added

--- rps.js
+++ rps.js
@@ -9,46 +9,65 @@ function getComputerChoice() {
     return choices[randomComputerChoiceNum];
 } 
 
-function game() {
-    for (let i =0; i < 5; i++) {
-        let playerSelection = prompt("Rock, Paper, or Scissors? 5 rounds against the computer!");
-        let playerChoice = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
-        const computerSelection = getComputerChoice();
-        alert(playRound(playerChoice, computerSelection));
+// function game() {
+//   for (let i =0; i < 5; i++) {
+//        let playerSelection = prompt("Rock, Paper, or Scissors? 5 rounds against the computer!");
+//        let playerChoice = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
+//        const computerSelection = getComputerChoice();
+//       alert(playRound(playerChoice, computerSelection));
+//
+//        if (i == 4) {
+//            alert(gameState());
+//        }
+//    }
+//}
 
-        if (i == 4) {
-            alert(gameState());
-        }
-    }
-}
 
+
+//logic: 3 buttons = 3 choices
+// each button is 1 round and correctly applies playerChoice parameter for playRound();
+const rock = document.getElementById('rockbtn');
+const paper = document.getElementById('paperbtn');
+const scissors = document.getElementById('scissorbtn');
+const playerSign= document.getElementById('results');
+
+rock.addEventListener('click', () => {
+    playRound('Rock', getComputerChoice());
+});
+paper.addEventListener('click', () => {
+    playRound('Paper', getComputerChoice());
+});
+scissors.addEventListener('click', () => {
+    playRound('Scissors', getComputerChoice());
+});
 
 function playRound (playerChoice, computerSelection) {
+
     if (playerChoice === computerSelection) {
-        return "Tie! Both chose the same";
+        playerSign.textContent = "Tie! Both chose the same";
     } 
     else if (playerChoice == "Rock" && computerSelection == "Paper") {
         computerScore+= 1;
-        return "You lose! Paper beats rock";
+        playerSign.textContent = "You lose! Paper beats rock";
     } 
     else if (playerChoice == "Paper" && computerSelection == "Rock") {
         playerScore+= 1;
-        return "You won! Paper beats rock";
+        playerSign.textContent = "You won! Paper beats rock";
     }
     else if (playerChoice == "Rock" && computerSelection == "Scissors") {
         playerScore+= 1;
-        return "You win! Rock beats scissors";
+        playerSign.textContent = "You win! Rock beats scissors";
     } 
     else if (playerChoice == "Scissors" && computerSelection == "Rock") {
         computerScore+= 1;
-        return "You lose! Rock beats scissors";
+        playerSign.textContent = "You lose! Rock beats scissors";
     }
     else if (playerChoice == "Paper" && computerSelection == "Scissors") {
         computerScore+= 1;
-        return "You lose! Scissors beat paper";
+        playerSign.textContent = "You lose! Scissors beat paper";
     } else {
         playerScore+= 1;
-        return "You win! Scissors beat paper";
+        playerSign.textContent = "You win! Scissors beat paper";
     }
 }
 
@@ -62,7 +81,7 @@ function gameState() {
         }
     }
 
-game();
+//game();
 //console.log(playerScore);
 //console.log(computerScore);
 //console.log(gameState());
